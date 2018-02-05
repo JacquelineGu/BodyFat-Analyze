21d3457d13d7905d8d5f28398be6e069481ed179
JacquelineGu <helen_gsr@outlook.com>
2018-02-05 12:59:39 -0600

    Add files via upload

--- code/lasso.R
+++ code/lasso.R
@@ -1,16 +1,7 @@
-library(dplyr)
-library(glmnet)
-library(ggplot2)
+source("./code/data.R")
 color1 = function(alpha=0.5){
   return(rgb(red = 0/255, green = 49/255, blue = 79/255, alpha = alpha))
 }
-## Data ##
-set.seed(100)
-dat <- read.csv("BodyFat.csv", header = T)
-dat <- dat[-c(39,42,182),-c(1,3)]
-train_index <- sample(1:249,size=200,replace=F)
-train <- dat[train_index,]
-test <- dat[-train_index,]
 ## Modeling ##
 full.model <- lm(BODYFAT~., train)
 X <- model.matrix(full.model)
@@ -23,7 +14,7 @@ mse <- (test$BODYFAT - Yhat)^2 %>% colMeans
 # color blind friendly palette
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 gg <- data.frame(log_lambda = log(l$lambda), mse = mse, df = l$df)
-gg$lasso <- "LASSO"
+gg$title <- "LASSO"
 for(i in length(gg$df):2){
   if(gg$df[i] == gg$df[i-1])
     gg$df[i] = NA
@@ -45,7 +36,7 @@ p <- ggplot(gg, aes(x=log_lambda, y=mse)) +
                  label=gg$df[!is.na(gg$df)]), 
              data = data.frame(gg$df[!is.na(gg$df)]),
              label.padding = unit(0.1, "lines")) 
-p <- p + facet_grid(. ~ lasso)+
+p <- p + facet_grid(. ~ title)+
   theme(strip.background = element_rect(fill=color1()),
         strip.text = element_text(size=15, colour="white"),
         panel.grid.major.x = element_blank(),
@@ -54,34 +45,39 @@ p <- p + facet_grid(. ~ lasso)+
 lambda <- l$lambda[39]
 coeff <- coef(l)[,39]
 coeff <- coeff[coeff!=0]
-min.mse <- (Yhat[,39]-test$BODYFAT)^2 %>% mean
+lasso.mse <- (Yhat[,39]-test$BODYFAT)^2 %>% mean
+lasso.result <- cbind(round(t(coeff),3),lambda=lambda,MSE=lasso.mse)
 
-
-data<-train
-m<-lm(BODYFAT ~ WEIGHT + ABDOMEN + WRIST,data = data)
-df=data.frame(p=predict(m),r=rstandard(m))
-df$lasso<-"Standardized Residual Plot"
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+m <- lm(BODYFAT ~ WEIGHT + ABDOMEN + WRIST,data = train)
+df <- data.frame(p=predict(m),r=rstandard(m))
+df$title<-"Standardized Residual Plot"
 d1<-ggplot(df,aes(p,r))+
-  geom_point(col =cbPalette[6],size=2,pch=23)+
+  geom_point(col =cbPalette[6],size=2,pch=20)+
   geom_hline(yintercept = 0,col="black",lwd=0.5)+
-  xlab("Predicted Body Fat %")+
+  xlab("Predicted BodyFat%")+
   ylab("Standardized Residuals")
-d1<-d1 + facet_grid(. ~ lasso)+
+d1<-d1 + facet_grid(. ~ title)+
   theme(strip.background = element_rect(fill=color1()),
            strip.text = element_text(size=15, colour="white"),
            panel.grid.major.x = element_blank(),
            panel.grid.minor.x = element_blank())
 
-y <- quantile(m$resid[!is.na(m$resid)], c(0.25, 0.75))
-x <- qnorm(c(0.25, 0.75))
-slope <- diff(y)/diff(x)
-int <- y[1L] - slope * x[1L]
+y <- sort(m$resid[!is.na(m$resid)])
+x <- qnorm((1:200-0.5)/200)
+qq <- data.frame(x=x, y=y)
+qq$title = "Normal Q-Q Plot"
+slope <- diff(quantile(y,c(.25,.75)))/(qnorm(0.75)-qnorm(0.25))
+int <- quantile(y,.25) - slope * qnorm(0.25)
 
-d2<-ggplot(m, aes(sample=.resid)) +
-  stat_qq(col =cbPalette[6],size=2,pch=21)+
-  geom_abline(slope = slope, intercept = int,col="black",lwd=0.5)+
-  ggtitle("Normal Q-Q Plot")
-
-
+d2<-ggplot(qq, aes(x=x,y=y)) +
+  geom_point(col =cbPalette[6],size=2,pch=20)+
+  geom_abline(slope = slope, intercept = int,
+              col="black",lwd=.8) +
+  xlab("Theoretical Quantile") +
+  ylab("Sample")
+d2<-d2 + facet_grid(. ~ title)+
+  theme(strip.background = element_rect(fill=color1()),
+        strip.text = element_text(size=15, colour="white"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank())
 
